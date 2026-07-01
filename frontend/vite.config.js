34ce564273965f0c9129742bf9754bbda8a234a5
yasminroch <yasmin.jesus@sou.inteli.edu.br>
2026-07-01 02:23:34 -0300

    vite configurarion deploy env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['/env.js'],
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
